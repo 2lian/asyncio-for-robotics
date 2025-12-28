48f11ac934df02d2147be1b076fc8b6992a6744d
Elian <elian.neppel@posteo.eu>
2025-12-28 15:47:58 +0900

    version bump and deleted init import due to deps issues

--- asyncio_for_robotics/__init__.py
+++ asyncio_for_robotics/__init__.py
@@ -1,12 +1,9 @@
 from .core.utils import soft_timeout, soft_wait_for, Rate
-from . import core, ros2, zenoh, textio 
+from .core.sub import BaseSub
 
 __all__ = [
     "soft_wait_for",
     "soft_timeout",
     "Rate",
-    "core",
-    "ros2",
-    "zenoh",
-    "textio",
+    "BaseSub",
 ]
