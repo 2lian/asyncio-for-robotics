cba2b55a4c5c60b11cd5cef44449ce90211b245e
Neppel Elian <elian.neppel@posteo.eu>
2026-02-17 15:23:57 +0100

    init update

--- asyncio_for_robotics/__init__.py
+++ asyncio_for_robotics/__init__.py
@@ -1,9 +1,10 @@
 from .core.utils import soft_timeout, soft_wait_for, Rate
-from .core.sub import BaseSub
+from .core.sub import BaseSub, ConverterSub
 
 __all__ = [
     "soft_wait_for",
     "soft_timeout",
     "Rate",
     "BaseSub",
+    "ConverterSub",
 ]
--- asyncio_for_robotics/textio/__init__.py
+++ asyncio_for_robotics/textio/__init__.py
@@ -1,4 +1,4 @@
-from ..core.utils import Rate, soft_timeout, soft_wait_for
+from .. import ConverterSub, Rate, soft_timeout, soft_wait_for
 from .sub import Sub, from_proc_stdout
 
 __all__ = [
@@ -6,6 +6,7 @@ __all__ = [
     "soft_wait_for",
     "soft_timeout",
     "Rate",
+    "ConverterSub",
     "Sub",
     "from_proc_stdout",
 ]
